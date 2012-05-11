0f4260161484b6ab2da479a553d32eae5310e53f
lunsher <lunsher@gmail.com>
2012-05-11 15:17:00 +0300

    Disabled deleyed job override

--- lib/sidekiq/extensions/active_record.rb
+++ lib/sidekiq/extensions/active_record.rb
@@ -17,11 +17,11 @@ module Sidekiq
       end
     end
 
-    module ActiveRecord
-      def delay
-        Proxy.new(DelayedModel, self)
-      end
-    end
+    #module ActiveRecord
+    #  def delay
+    #    Proxy.new(DelayedModel, self)
+    #  end
+    #end
 
   end
 end
